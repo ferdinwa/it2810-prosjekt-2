21f285bb60607efc629a84ae13c2993aab8c2c99
eivindsd <eivindsd@stud.ntnu.no>
2020-09-10 10:47:19 +0200

    remove extra App.tsx

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Header from "./components/Header";
-import Navbar from "./components/Navbar"
+import Header from "./Header";
+import Navbar from "./Navbar"
 import { render } from '@testing-library/react';
-
 
 
 export default class App extends React.Component {
