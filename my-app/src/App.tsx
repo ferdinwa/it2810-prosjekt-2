497dae54b3cf5f89f1ef68fbbdfdf9b4d7787bee
eivindsd <eivindsd@stud.ntnu.no>
2020-09-10 09:28:54 +0200

    make-header

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar"
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App" data-observe-resizes>
+        <Navbar />
+        <Header />
+      </div>
+    );
+  }
 }
 
-export default App;
+
---
+++ my-app/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+
+export default function Navbar() {
+
+    function loveTheme() {
+        document.body.style.backgroundColor = "pink"
+        document.body.style.color = "red"
+    }
+    function happyTheme() {
+        document.body.style.backgroundColor = "lightblue"
+        document.body.style.color = "magenta"
+    }
+    function sadTheme() {
+        document.body.style.backgroundColor = "grey"
+        document.body.style.color = "lightgrey"
+    }
+
+    function summerTheme() {
+        document.body.style.backgroundColor = "yellow"
+        document.body.style.color = "orange";
+    }
+
+    return (
+        <div>
+            <ul id="nav">
+                <li><a href="#" onClick={loveTheme}>Kjærleik</a></li>
+                <li><a href="#" onClick={happyTheme}>God stemning</a></li>
+                <li><a href="#" onClick={sadTheme}>Trist</a></li>
+                <li><a href="#" onClick={summerTheme}>Sommer</a></li>
+              </ul>
+        </div>
+    )
+}
+
+
