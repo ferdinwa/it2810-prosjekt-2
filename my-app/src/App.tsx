bd0c91c5027cc7fc6170035cda8f5fe960e46a8a
Eirik Olav Aa <eirioa@stud.ntnu.no>
2020-09-17 12:02:34 +0200

    Pictures and header are merged

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header";
 import './App.css';
+import ImageSlider from './components/ImageSlider';
 
 export default class App extends React.Component {
 
@@ -9,6 +10,7 @@ export default class App extends React.Component {
     return (
       <div className="App" data-observe-resize>
         <Header />
+        <ImageSlider />
       </div>
     );
   }
