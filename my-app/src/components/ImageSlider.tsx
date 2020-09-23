914b029629fcc14ac50714746ab8743043be6590
Ferdinand Ward Ådlandsvik <ferdinwa@stud.ntnu.no>
2020-09-23 15:20:27 +0200

    Fixed session-storage for pictures

--- my-app/src/components/ImageSlider.tsx
+++ my-app/src/components/ImageSlider.tsx
@@ -1,29 +1,50 @@
 import React from 'react'
 import './Frame.css';
 
-export default class ImageSlider extends React.Component {
-    state = {
-        images: [
-            "/whale.svg",
-            "/summer.svg",
-            "/heart.svg"
+interface ImageSliderState {
+    images: string[]
+    displayed_picture: string
+    index: number
+}
 
-        ],
-        index: 0
-    };
+export default class ImageSlider extends React.Component<any, ImageSliderState> {
+    
+    constructor(props: any){
+        super(props)
+        this.state = {
+            images: [
+                "whale.svg",
+                "summer.svg",
+                "heart.svg"
+            ],
+            index: Number(sessionStorage.getItem("index")) || 0,
+            displayed_picture: sessionStorage.getItem("picture") || ""}
+    }
 
     handleNext = () => {
         if (this.state.index === 2) {
+            sessionStorage.setItem("index", "0")
             this.setState({
-                index: 0
+                index: Number(sessionStorage.getItem("index"))
             })
         }
-        else {
+        else if (this.state.index === 1) {
+            sessionStorage.setItem("index", "2")
             this.setState({
-                index: this.state.index + 1
+                index: Number(sessionStorage.getItem("index"))
             })
         }
+        else if (this.state.index === 0) {
+            sessionStorage.setItem("index", "1")
+            this.setState({index: Number(sessionStorage.getItem("index"))})
+        }
+        //Saves the picture that is displayed to session-storage
+        sessionStorage.setItem("picture", this.state.images[this.state.index])
+
+        this.setState({displayed_picture: sessionStorage.getItem("picture")!})
+        
         console.log(this.state.index)
+        console.log(sessionStorage.getItem("picture"))
 
     }
 
@@ -33,7 +54,7 @@ export default class ImageSlider extends React.Component {
                 <div className="container frame">
                     <img
                         className="image"
-                        src={this.state.images[this.state.index]}
+                        src={this.state.displayed_picture}
                         alt="" />
                     <p className="p">Halla</p>
 
