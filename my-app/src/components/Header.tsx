f5bb767c695e9b2be6b29722963dc96cc841a822
eivindsd <eivindsd@stud.ntnu.no>
2020-09-23 08:51:13 +0200

    Put state logic and localstorage in App.tsx

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -6,16 +6,14 @@ export default function Header(props: any): React.ReactElement {
 
     const [isNavVisible, setIsNavVisible] = useState(true);
     const [isSmallScreen, setIsSmallScreen] = useState(false);
-    const [background, setBackground] = useState<string>(localStorage.getItem("background") || "");
-    const [color, setColor] = useState<string>(props.color);
+    //const [background, setBackground] = useState<string>(localStorage.getItem("background") || "");
+    //const [color, setColor] = useState<string>(props.color);
 
-    useEffect(() => 
-        setBackground(background), [background]);
     
     useEffect(() => {
-        document.body.style.background = background;
-        document.body.style.color = color;
-        }, [background, color]);
+        document.body.style.background = props.background;
+        document.body.style.color = props.color;
+        }, [props.background, props.color]);
     
 
     useEffect(() => {
@@ -40,49 +38,24 @@ export default function Header(props: any): React.ReactElement {
         setIsNavVisible(!isNavVisible);
     };
 
-    const handleLoveClick = () => {
-        setBackground("pink url('heart.svg') no-repeat 50% 25%")
-        setColor("red");
-    }
-
-    const handleWhaleClick = () => {
-        setBackground("lightblue url('whale.svg') no-repeat 50% 40%")
-        setColor("blue");  
-    }
-
-    const handleSunClick = () => {
-        setBackground("lightyellow url('summer.svg') no-repeat 50% 40%")
-        setColor("orange")
-        
-    }
-
-    const handleLogoClick = () => {
-        setBackground("white url('artist.svg') no-repeat 50% 40%")
-        setColor("black")
-    }
-
-    const handleSaveClick = () => {
-        localStorage.setItem("background", background);
-    }
-
 
 
     return (
         <div className="grid-container">
             <div className="Navbar">
-                <img src={require("../pictures/logo192.png")} className="Logo" alt="logo" onClick={handleLogoClick} />
+                <img src={require("../pictures/logo192.png")} className="Logo" alt="logo" onClick={props.handleLogoClick} />
                 {(!isSmallScreen || isNavVisible) && (
                     <nav className="Nav">
-                        <button className="theme" id="love" onClick={handleLoveClick}>&#128150;</button>
-                        <button className="theme" id="whale" onClick={handleWhaleClick}>&#128011;</button>
-                        <button className="theme" id="sun" onClick={handleSunClick}>&#128526;</button>
-                        <button className="save" onClick={handleSaveClick}>Lagre</button>
+                        <button className="theme" id="love" onClick={props.handleLoveClick}>&#128150;</button>
+                        <button className="theme" id="whale" onClick={props.handleWhaleClick}>&#128011;</button>
+                        <button className="theme" id="sun" onClick={props.handleSunClick}>&#128526;</button>
+                        <button className="save" onClick={props.handleSaveClick}>Lagre</button>
                     </nav>
                 )}
                 <button onClick={toggleNav} className="Lion">&#129409;</button>
             </div>
             <div className="Title">
-                <h1>Kul utstilling</h1>
+                <h1>{props.title}</h1>
             </div>
         </div>
     )
